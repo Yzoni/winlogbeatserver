fd8c5ed69abb0a6ac2e882f1257e03ce93dfd275
Yzoni <mail@yorickdeboer.nl>
2019-11-15 14:07:07 +0100

    update

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup
 setup(
     name="winlogbeatserver",
     version='1.0.0',
-    url="https://yrck.nl"
+    url="https://yrck.nl",
+    author='Y. de Boer',
+    packages=['winlogbeatserver'],
 )
